5ca7c0d6c039101be1ddd0395e2ae01a1f6e8ef3
mostafizurahmed8200 <mostafizur.ahmed@jayamsolutions.com>
2024-04-03 12:06:08 +0530

    latest

---
+++ lib/page/cart_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CartItemPage extends StatefulWidget {
+  const CartItemPage({super.key});
+
+  @override
+  State<CartItemPage> createState() => _CartItemPageState();
+}
+
+class _CartItemPageState extends State<CartItemPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
